a4b099d6b35eabb391f4cf06718801dcd5a5bbf3
SilvenLEAF <SilvenLEAF@gmail.com>
2020-12-04 16:01:10 +0530

    Prepared for Heroku Deploy

--- app.ts
+++ app.ts
@@ -14,6 +14,18 @@ app.use(express.static(path.join(__dirname, `client/build`)));
 
 
 
+/* ----------------------------------
+.             routes
+---------------------------------- */
+app.get('*', (req, res, next)=>{
+  res.sendFile(path.join(__dirname, `client/build/index.html`));  
+})
+
+
+
+
+
+
 // -------------------LISTEN
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
